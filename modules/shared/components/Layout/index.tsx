9bf9878a5ea5aacae6b9600b6f1f98b1d55069da
atwfk <ahmed.twfiek8@gmail.com>
2022-02-23 16:24:51 +0200

    install react-toastify

--- modules/shared/components/Layout/index.tsx
+++ modules/shared/components/Layout/index.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import type { FC, ReactElement } from "react";
 import Navbar from "../Navbar";
 import { StyledMain } from "./styles";
+import { ToastContainer, toast } from "react-toastify";
+
+toast.configure();
 
 const Layout: FC = ({ children }): ReactElement => {
   return (
     <>
+      <ToastContainer limit={2} />
       <Navbar />
       <StyledMain>{children}</StyledMain>
     </>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 import type { AppProps } from "next/app";
 import type { ReactElement } from "react";
 import Layout from "@modules/shared/components/Layout";

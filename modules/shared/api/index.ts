4836e56a51e02435f642220c1926e9f61dc79bfb
atwfk <ahmed.twfiek8@gmail.com>
2022-02-23 16:46:54 +0200

    added base url to prod and dev env files

--- modules/shared/api/index.ts
+++ modules/shared/api/index.ts
@@ -1,15 +1,8 @@
 import axios from "axios";
-import type { AxiosRequestConfig } from "axios";
 
 export const api = axios.create({
-  baseURL: "https://dummyapi.io/data/v1",
-});
-
-api.interceptors.request.use(
-  (config: AxiosRequestConfig) => {
-    const { headers } = config;
-    headers!["app-id"] = process.env.NEXT_PUBLIC_APP_ID ?? "";
-    return config;
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  headers: {
+    "app-id": process.env.NEXT_PUBLIC_APP_ID as string,
   },
-  async (e: Error) => Promise.reject(e),
-);
+});
